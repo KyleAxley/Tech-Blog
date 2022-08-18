e3dc66844fd930edfa72b9ceb06af371ec6d5dea
KyleAxley <kanderson7645@yahoo.com>
2022-08-18 17:23:39 -0400

    connection/ schema created

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+//import for sequelize constructor
+const Sequelize = require('sequelize');
+
+//require connection for .env file
+require("dotenv").config();
+
+//connection to database
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3006
+    });
+}
+
+//export for connection to database
+module.exports = sequelize;
