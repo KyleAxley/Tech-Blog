f5965793a63bbded3b099bf34ac32798a16cfc1c
KyleAxley <kanderson7645@yahoo.com>
2022-08-22 14:06:25 -0400

    added some styling

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -15,15 +15,45 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
-        console.log('success');
+      console.log("success");
       document.location.replace("/dashboard");
     } else {
       alert(response.statusText);
     }
   }
-  console.log('confirmed click');
+  console.log("confirmed click");
+}
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (!username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
 }
 
 document
   .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector(".signup-form")
   .addEventListener("submit", loginFormHandler);
